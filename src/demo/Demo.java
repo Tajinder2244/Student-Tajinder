aab9758bbb66fde9dc25295ef235c0b2af08c31a
Tajinder2244 <Sing8580@sheridancollege.ca>
2019-05-22 14:16:47 -0400

    Student Demo

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -15,7 +15,8 @@ public class Demo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+     
+        // Tajinder TODO code application logic here
     }
     
 }
